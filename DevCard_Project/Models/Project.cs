7e39a284ddc851b25f0a379b4c2455884330204a
mobina <mobina@DESKTOP-B49L8SI>
2022-08-16 00:05:02 +0430

    project section from index page moved partial view Projets

---
+++ DevCard_Project/Models/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_Project.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Clident { get; set; }
+    }
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+
+}
